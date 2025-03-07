0ad3fe082f4ca58ab44b1eba6c9d3e8a76b6bdd8
Radim Hopp <rhopp@redhat.com>
2025-03-07 09:11:12 +0100

    back back

--- integration-tests/scripts/rhtap-e2e-runner.sh
+++ integration-tests/scripts/rhtap-e2e-runner.sh
@@ -94,7 +94,7 @@ configure_image_registry() {
     log "INFO" "Setting up image registry configuration"
     
     # Set default organization
-    export IMAGE_REGISTRY_ORG="rhtap"
+    export IMAGE_REGISTRY_ORG="rhtap_qe"
     
     # Check for Quay integration
     if secret_exists "rhtap" "rhtap-quay-integration"; then
